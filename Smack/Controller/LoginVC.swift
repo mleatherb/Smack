ac5b5e24200a894c710f9d60fdf0ac907096305d
Michael Leatherbury <mike.leatherbury@mintek.com>
2018-04-18 14:58:32 -0400

    added table view for channels. Login vc and segues

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Michael Leatherbury on 4/18/18.
+//  Copyright © 2018 Michael Leatherbury. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Michael Leatherbury on 4/18/18.
+//  Copyright © 2018 Michael Leatherbury. All rights reserved.
+//
+
+import Foundation
+
+
+// Segues
+
+let TO_LOGIN = "toLogin"
